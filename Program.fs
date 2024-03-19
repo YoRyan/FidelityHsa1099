e4324d28fbf1cedebad6c77103d3f219c6402a9e
Ryan Young <ryan@youngryan.com>
2024-03-19 20:12:08 +0000

    feat: calculate interest and dividends

--- Program.fs
+++ Program.fs
@@ -63,25 +63,61 @@ let readTransactions (rows: seq<CsvRow>) =
         | _ -> None)
         rows
 
+let getDate =
+    function
+    | Purchase(_, _, _, d) -> d
+    | Sell(_, _, _, d) -> d
+    | Dividend(_, _, d) -> d
+    | Interest(_, d) -> d
+
+let filterFor (transactions: seq<Transaction>) (year: int) =
+    Seq.filter
+        (fun tx ->
+            let date = getDate tx
+            date >= DateOnly(year, 1, 1) && date <= DateOnly(year, 12, 31))
+        transactions
+
+let interestFor (transactions: seq<Transaction>) (year: int) =
+    filterFor transactions year
+    |> Seq.choose (function
+        | Interest(amount, _) -> Some amount
+        | _ -> None)
+    |> Seq.sum
+
+let dividendsFor (transactions: seq<Transaction>) (year: int) =
+    let folder (state: Map<string, float>) ((symbol, amount): string * float) =
+        let sum =
+            match Map.tryFind symbol state with
+            | Some s -> s
+            | None -> 0
+
+        Map.add symbol (sum + amount) state
+
+    filterFor transactions year
+    |> Seq.choose (function
+        | Dividend(symbol, amount, _) -> Some(symbol, amount)
+        | _ -> None)
+    |> Seq.fold folder Map.empty<string, float>
+
+let cashf = sprintf "$ %.2f"
+
 [<EntryPoint>]
 let main args =
-    let csvPath =
+    let csvPath, year =
         match args with
-        | [| a0 |] -> a0
-        | _ -> failwith "Expected arguments: <csv file>"
+        | [| a0; a1 |] -> a0, int a1
+        | _ -> failwith "Expected arguments: <csv file> <year>"
 
     let csvRows = CsvFile.Load(csvPath, ignoreErrors = true).Rows
 
     let orderedTransactions =
-        readTransactions csvRows
-        |> Seq.toList
-        |> List.sortBy (function
-            | Purchase(_, _, _, d) -> d
-            | Sell(_, _, _, d) -> d
-            | Dividend(_, _, d) -> d
-            | Interest(_, d) -> d)
+        readTransactions csvRows |> Seq.toList |> List.sortBy getDate
 
-    for tx in orderedTransactions do
-        printfn "%s" (tx.ToString())
+    let dividends = dividendsFor orderedTransactions year
+    printfn "Dividends Received:"
+    Map.iter (fun symbol amount -> printfn "%s:\t%s" symbol (cashf amount)) dividends
+
+    let interest = interestFor orderedTransactions year
+    printfn "\nInterest Earned:\t%s" (cashf interest)
 
     0
